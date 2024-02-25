5be972962efcde83fefa562a576e5be7ff9dc0f3
Aniol Quer <aniolquer@gmail.com>
2024-02-25 12:48:07 +0100

    updated copy again

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,9 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <footer className="px-4 mb-10 text-center text-gray-500">
-      <small className="block mb-2 text-xs">
-        &copy; 2024 Aniol Quer. All rights reserved.
-      </small>
+      <small className="block mb-2 text-xs">&copy; 2024 Aniol Quer.</small>
     </footer>
   );
 }
