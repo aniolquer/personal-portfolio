7e4130a3bb6b9484de29756c7768a696e5a5dc60
Aniol Quer <aniolquer@gmail.com>
2024-02-27 19:45:31 +0100

    renewed email

--- next.config.js
+++ next.config.js
@@ -8,14 +8,9 @@ const nextConfig = {
       },
     ],
   },
-  output: "export",
-  // experimental: {
-  //   serverActions: true,
-  // },
-  // Add the environment check and repository name here
-  assetPrefix:
-    process.env.NODE_ENV === "production" ? "/aniolquer.github.io" : "",
-  basePath: process.env.NODE_ENV === "production" ? "/aniolquer.github.io" : "",
+  experimental: {
+    serverActions: true,
+  },
 };
 
 module.exports = nextConfig;
